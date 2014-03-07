2d41f6bbbfd370aa011a6d19811d0c9c98a4861b
Imad Mouhtassem <mouhtasi@gmail.com>
2014-03-07 17:28:09 -0500

    Added function to sort dict to list of tuples.

--- huff.py
+++ huff.py
@@ -2,8 +2,9 @@
 
 import sys
 import os
+from operator import itemgetter
 
-def get_frequency_dict(filename):
+def frequency_dict(filename):
     '''Read the file and create a dictionary with the read item and its
     frequency.'''
     freq = {}
@@ -18,10 +19,20 @@ def get_frequency_dict(filename):
 
     return freq
 
+def dict_to_tuples(freq_dict):
+    '''Convert frequency dict to a list of tuples.'''
+    freq = []
+
+    for item, value in freq_dict.items():
+        freq.append((item, value))
+
+    return sorted(freq, key=itemgetter(1))
+
 if __name__ == '__main__':
     if len(sys.argv) == 1:
         print('Usage: python3 huff.py <filename>')
         sys.exit(2)
     elif len(sys.argv) > 1:
         filename = sys.argv[1]
-        freq = get_frequency_dict(filename)
+        freq_dict = frequency_dict(filename)
+        freq_list = dict_to_tuples(freq_dict)
